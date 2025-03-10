0104c1f05571fbc1ef8d0b32024290a4e556b921
Jeremy Barthelemy <j.l.barthel@gmail.com>
2025-03-09 21:51:53 -0400

    Add fun background music!

--- miniprojects/BalloonPopper/BalloonManager.gd
+++ miniprojects/BalloonPopper/BalloonManager.gd
@@ -3,6 +3,9 @@ extends Node3D
 var score : int = 0
 @export var score_text : Label
 
+func _ready():
+	$BackgroundMusic.play()
+	
 func increase_score(amount):
 	score += amount
 	score_text.text = str("Score: ", score)
